bd968b693ce29e3c9d09307116a236ae683337ee
xiushak <xiebrandon@gmail.com>
2024-07-15 17:30:21 -0700

    basic idea of how main page looking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
+import Link from "next/link";
+
+type ProjectCardInfo = {
+  title: string;
+  link: string;
+  description: string;
+  thumbnail: string;
+};
+
 export default function Main() {
+  const project_map: ProjectCardInfo[] = [
+    {
+      title: "Faculty Activity Tracker",
+      link: "/projects/faculty-activity-tracker",
+      description: "if wanted",
+      thumbnail: "",
+    },
+    {
+      title: "Penella",
+      link: "/projects/penella",
+      description: "if wanted",
+      thumbnail: "",
+    },
+  ];
+
   return (
     <main>
       <h1 className="p-2">Main</h1>
-      <p className="p-2">
-        This is the main content area. It's where the main content goes.
-      </p>
+      <ul className="p-2">
+        {project_map.map((project) => (
+          <li
+            className="m-2 p-1 border border-black rounded-md"
+            key={project.title}
+          >
+            <Link href={project.link}>
+              <h3>{project.title}</h3>
+              <img src={project.thumbnail} alt={project.title} />
+              <p className="pl-2">{project.description}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }

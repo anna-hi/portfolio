0c8cd200d8e1e20fcca25bcdd12cbd7735073863
xiushak <xiebrandon@gmail.com>
2025-04-07 21:20:07 -0700

    wip

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,24 @@
+import styles from "./style.module.css";
+
 export default function About() {
   return (
-    <div className="p-2">
-      <h1>About</h1>
+    <div className={styles.container}>
+      <div>
+        <h1>
+          A little about <i className="text-pink">Anna.</i>
+        </h1>
+        <p>
+          Hey, I’m Anna. I’m a designer with a background in Computer Science
+          and Design. I focus primarily on product and user experience, and I’m
+          passionate about designing products that make a noticeable impact on
+          real people’s lives.
+        </p>
+        <p>
+          I’m currently pursuing a Masters degree in Human Computer Interaction
+          at Carnegie Mellon University and recently just graduated summa cum
+          laude from Northeastern University.
+        </p>
+      </div>
     </div>
   );
 }
